c372dad27ff60643571065d0d07adfe442a515f7
PaulJC79 <PaulJC79@Googlemail.Com>
2014-06-01 12:04:10 +0100

    New Design

--- assets/www/js/ot-app.js
+++ assets/www/js/ot-app.js
@@ -1,8 +1,8 @@
 // global variables
 var db;
-var shortName = 'WebSqlDB';
+var shortName = 'OT_DB';
 var version = '1.0';
-var displayName = 'WebSqlDB';
+var displayName = 'OT_DB';
 var maxSize = 65535;
 
 // this is called when an error happens in a transaction
@@ -27,40 +27,30 @@ function onBodyLoad(){
     alert("DEBUGGING: we are in the onBodyLoad() function");
 
     if (!window.openDatabase) {
-    // not all mobile devices support databases  if it does not, the
-    following alert will display
+    // not all mobile devices support databases  if it does not, the following alert will display
     // indicating the device will not be albe to run this application
     alert('Databases are not supported in this browser.');
     return;
     }
 
 // this line tries to open the database base locally on the device
-// if it does not exist, it will create it and return a database
-    object stored in variable db
+// if it does not exist, it will create it and return a database object stored in variable db
     db = openDatabase(shortName, version, displayName,maxSize);
 
-// this line will try to create the table User in the database just
-    created/openned
+// this line will try to create the table User in the database just created/openned
     db.transaction(function(tx){
 
-    // you can uncomment this next line if you want the User table to be
-    empty each time the application runs
+    // you can uncomment this next line if you want the User table to be empty each time the application runs
     // tx.executeSql( 'DROP TABLE User',nullHandler,nullHandler);
 
-    // this line actually creates the table User if it does not exist
-    and sets up the three columns and their types
-    // note the UserId column is an auto incrementing column which is
-    useful if you want to pull back distinct rows
+    // this line actually creates the table User if it does not exist and sets up the three columns and their types
+    // note the UserId column is an auto incrementing column which is useful if you want to pull back distinct rows
     // easily from the table.
-    tx.executeSql( 'CREATE TABLE IF NOT EXISTS User(UserId INTEGER NOT
-    NULL PRIMARY KEY, FirstName TEXT NOT NULL, LastName TEXT NOT NULL)',
-    [],nullHandler,errorHandler);
-    },errorHandler,successCallBack);
+    tx.executeSql( 'CREATE TABLE IF NOT EXISTS User(UserId INTEGER NOT NULL PRIMARY KEY, FirstName TEXT NOT NULL, LastName TEXT NOT NULL)',[],nullHandler,errorHandler);},errorHandler,successCallBack);
 
     }
 
-// list the values in the database to the screen using jquery to
-update the #lbUsers element
+// list the values in the database to the screen using jquery to update the #lbUsers element
 function ListDBValues() {
 
     if (!window.openDatabase) {
@@ -68,15 +58,12 @@ function ListDBValues() {
     return;
     }
 
-// this line clears out any content in the #lbUsers element on the
-    page so that the next few lines will show updated
+// this line clears out any content in the #lbUsers element on the page so that the next few lines will show updated
 // content and not just keep repeating lines
     $('#lbUsers').html('');
 
-// this next section will select all the content from the User table
-    and then go through it row by row
-// appending the UserId  FirstName  LastName to the  #lbUsers element
-    on the page
+// this next section will select all the content from the User table and then go through it row by row
+// appending the UserId  FirstName  LastName to the  #lbUsers element on the page
     db.transaction(function(transaction) {
     transaction.executeSql('SELECT * FROM User;', [],
     function(transaction, result) {
@@ -94,8 +81,7 @@ function ListDBValues() {
 
     }
 
-// this is the function that puts values into the database using the
-values from the text boxes on the screen
+// this is the function that puts values into the database using the values from the text boxes on the screen
 function AddValueToDB() {
 
     if (!window.openDatabase) {
@@ -103,16 +89,11 @@ function AddValueToDB() {
     return;
     }
 
-// this is the section that actually inserts the values into the User
-    table
+// this is the section that actually inserts the values into the User table
     db.transaction(function(transaction) {
-    transaction.executeSql('INSERT INTO User(FirstName, LastName)
-    VALUES (?,?)',[$('#txFirstName').val(), $('#txLastName').val()],
-    nullHandler,errorHandler);
-    });
+    transaction.executeSql('INSERT INTO User(FirstName, LastName) VALUES (?,?)',[$('#txFirstName').val(), $('#txLastName').val()], nullHandler,errorHandler);});
 
-// this calls the function that will show what is in the User table in
-    the database
+// this calls the function that will show what is in the User table in the database
     ListDBValues();
 
     return false;
