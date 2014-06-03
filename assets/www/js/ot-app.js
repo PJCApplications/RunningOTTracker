5a77c12d7eaa1220a280a98fe9c484b4676da147
PaulJC79 <PaulJC79@Googlemail.Com>
2014-06-03 10:18:08 +0100

    New Design

--- assets/www/js/ot-app.js
+++ assets/www/js/ot-app.js
@@ -127,4 +127,44 @@ function error (transaction, err)
         return false;
         }
 
+function timestamp() {
+
+    var d = new Date();
+
+    var year = d.getFullYear();
+
+    var month = d.getMonth();
+    if (month <10)
+    {
+    month = "0" + month;
+    }
+
+    var date = d.getDate();
+    if (date < 10)
+    {
+    date = "0" + date;
+    }
+
+    var hours = d.getHours();
+    if (hours < 10)
+    {
+    hours = "0" + hours;
+    }
+
+    var minutes = d.getMinutes();
+    if (minutes < 10)
+    {
+    minutes = "0" + minutes;
+    }
+
+    var seconds = d.getSeconds();
+    if (seconds < 10)
+    {
+    seconds = "0" + seconds;
+    }
+
+
+    document.getElementById("timestamp").innerHTML = year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds;
+    }
+
 </script>
